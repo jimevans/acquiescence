f86f3a6bc292a3b51db2a61693c67feb5a892b63
Jim Evans <james.h.evans.jr@gmail.com>
2025-12-01 15:34:45 -0500

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import vitest from 'eslint-plugin-vitest';
 
 export default tseslint.config(
   // Ignore patterns first
